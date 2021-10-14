418524c4e1689dd2662822b43af274fcacbbe603
Jacob Stephens <jacob@stewardgoods.com>
2021-10-14 15:08:29 -0400

    add custom favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,11 @@ function Home() {
     <Helmet>
       <title>KLS Wedding Coordination</title>
       <meta name="description" content="Lancaster County wedding coordination services."></meta>
-      </Helmet>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+      <link rel="manifest" href="/site.webmanifest"></link>
+    </Helmet>
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="#home">KLS <div className="elegant">Wedding Coordination</div></Navbar.Brand>
